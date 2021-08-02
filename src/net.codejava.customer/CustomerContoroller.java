243817f322c5d6b53f41076530a3e0d46d4a0d0b
modewed <vg592@ya.ru>
2021-08-02 19:20:00 +0500

    fix

--- src/net.codejava.customer/CustomerContoroller.java
+++ src/net.codejava.customer/CustomerContoroller.java
@@ -2,17 +2,23 @@ package net.codejava.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 
 @Controller
-public class CustomerController {
+public class CustomerContoroller {
 
     @Autowired
     private CustomerService customerService;
 
     @RequestMapping("/")
+
     public ModelAndView home() {
         List<Customer> listCustomer = customerService.listAll();
+
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("listCustomer", listCustomer);
         return mav;
